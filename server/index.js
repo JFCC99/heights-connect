b83adf738ffe27ac716f2764c47900775ace113e
Johan Cedeno <data4johan@gmail.com>
2026-04-12 15:42:25 -0400

    Initial setup - server, MongoDB connection, packages

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const path = require('path')
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Test route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Heights Connect server is running!' })
+})
+
+// Connect to MongoDB and start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log('MongoDB connection error:', err)
+  })
